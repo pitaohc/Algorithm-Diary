245b92fac9bc2ef57ebdb92b35f05c652cdece41
pitao <pitaohc@qq.com>
2022-12-02 11:35:05 +0800

    70. 爬楼梯

---
+++ LC-70/Solution.cpp
@@ -0,0 +1,20 @@
+﻿#include "Solution.h"
+#include"Solution.h"
+#include<vector>
+using namespace std;
+
+int Solution::climbStairs(int n)
+{
+    if (n == 1) return 1;
+    else if (n == 2) return 2;
+
+    int n_1 = 2, n_2 = 1;
+    int n = 0;
+    for (size_t i = 3; i <= n; i++)
+    {
+        n = n_1 + n_2;
+        n_2 = n_1;
+        n_1 = n;
+    }
+    return n;
+}
---
+++ LC-70/Solution.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include<vector>
+
+class Solution {
+public:
+    int climbStairs(int n);
+};
