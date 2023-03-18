ddef7afafbb6ebae5f4d632c2f6b18828b77b0b2
pitao <pitaohc@qq.com>
2023-03-18 22:05:33 +0800

    力扣455 https://leetcode.cn/problems/assign-cookies/

---
+++ LC-455/main.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include"stdc++.h"
+#include"Solution.h"
+using namespace std;
+int main()
+{
+    Solution sol;
+    vector<int> g, s;
+
+    //case 1
+    g = { 1,2,3 };
+    s = { 1,1 };
+    cout << sol.findContentChildren(g, s) << endl;
+
+    g = { 1,2 };
+    s = { 1,2,3 };
+    cout << sol.findContentChildren(g, s) << endl;
+    return 0;
+}
---
+++ LC-455/stdc++.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<algorithm>
