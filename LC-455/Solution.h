6ee4fa2a2b5f426edb2a98693e9c1a4e3df87123
pitao <pitaohc@qq.com>
2023-03-18 22:05:33 +0800

    455. 分发饼干 https://leetcode.cn/problems/assign-cookies/

---
+++ LC-455/Solution.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include"stdc++.h"
+
+class Solution {
+
+public:
+    int findContentChildren(std::vector<int>& g, std::vector<int>& s) {
+        using namespace std;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int indexG = 0, indexS = 0;
+        while (indexG < g.size() && indexS < s.size())
+        {
+            if (g[indexG] <= s[indexS]) //如果能满足小朋友胃口
+            {
+                ++indexG;
+            }
+            ++indexS;
+        }
+        return indexG;
+    }
+};
