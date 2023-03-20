30aeeee54ee471043f35cfde495e9711d45ba08d
pitao_hc <HarveyCLC@outlook.com>
2023-03-20 15:30:41 +0800

    435. 无重叠区间 https://leetcode.cn/problems/non-overlapping-intervals/

---
+++ LeetCode/LC-435/Solution.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include"stdc++.h"
+
+class Solution {
+public:
+    int eraseOverlapIntervals(std::vector<std::vector<int>>& intervals) {
+        int n = intervals.size();
+        if (n == 0) return 0;
+
+        sort(intervals.begin(), intervals.end(), [](const auto& left, const auto& right) {
+            return left[1] < right[1];
+            });
+
+        int right = intervals[0][1];
+        int result = 1;
+        for (size_t i = 1; i < n; i++)
+        {
+            if (intervals[i][0] >= right)
+            {
+                ++result;
+                right = intervals[i][1];
+            }
+        }
+        return n - result;
+    }
+};
---
+++ LeetCode/LC-435/main.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include"stdc++.h"
+#include"Solution.h"
+using namespace std;
+
+template<class T>
+ostream& operator<<(ostream& out, const vector<T>& vect)
+{
+    out << "[";
+    for (const auto& elem : vect)
+    {
+        out << elem << ", ";
+    }
+    out << "\b\b]";
+    return out;
+}
+
+
+int main()
+{
+    Solution sol;
+    vector<vector<int>> intervals;
+    //case 1
+    cout << "Case 1\n";
+    intervals = { {1,2},{2,3},{3,4},{1,3}, };//[[1,2],[2,3],[3,4],[1,3]]
+    cout << intervals << endl;
+    cout << "Result: " << sol.eraseOverlapIntervals(intervals) << endl;
+    cout << "Answer: " << 1 << endl << endl;
+
+    //case 2
+    cout << "Case 2\n";
+    intervals = { {1,2},{1,2},{1,2}, };//[[1,2],[2,3],[3,4],[1,3]]
+    cout << intervals << endl;
+    cout << "Result: " << sol.eraseOverlapIntervals(intervals) << endl;
+    cout << "Answer: " << 2 << endl << endl;
+
+    //case 3
+    cout << "Case 3\n";
+    intervals = { {1,2},{2,3}, };//[[1,2],[2,3],[3,4],[1,3]]
+    cout << intervals << endl;
+    cout << "Result: " << sol.eraseOverlapIntervals(intervals) << endl;
+    cout << "Answer: " << 0 << endl << endl;
+
+
+    return 0;
+}
