b2a7c97d07cdf5aad9dd637521dc5e0b88b9721e
pitao_hc <HarveyCLC@outlook.com>
2023-03-21 12:20:53 +0800

    605. 种花问题 https://leetcode.cn/problems/can-place-flowers/

---
+++ LeetCode/LC-605/Solution.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include"stdc++.h"
+
+class Solution {
+public:
+    bool canPlaceFlowers(std::vector<int>& flowerbed, int n) {
+
+        int i = 0;
+        while (i < flowerbed.size())
+        {
+            if (flowerbed[i] == 1)
+            {
+                //当前位置已经有花
+                i += 2; //下一个位置肯定不能种
+            }
+            else
+            {
+                bool next = (i + 1 == flowerbed.size() || i + 1 < flowerbed.size() && flowerbed[i + 1] == 0);
+
+                if (next)
+                {
+                    n--;
+                    i += 2;
+                }
+                else
+                {
+                    i += 3;
+                }
+            }
+        }
+        return n <= 0;
+    }
+};
---
+++ LeetCode/LC-605/main.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include"stdc++.h"
+#include"Solution.h"
+#include"util.h"
+using namespace std;
+
+
+int main()
+{
+    Solution sol;
+    vector<int>flowerbed;
+    int n;
+    bool result;
+    bool answer;
+    //case 1
+    flowerbed = { 1,0,0,0,1 };
+    n = 1;
+    result = sol.canPlaceFlowers(flowerbed, n);
+    answer = true;
+    cout << "Case " << 1 << endl
+        << "flowered:" << flowerbed << endl
+        << "n = " << n << endl
+        << "result = " << result << endl
+        << "answer = " << answer << endl
+        << endl;
+    //case 2
+    flowerbed = { 1,0,0,0,1 };
+    n = 2;
+    result = sol.canPlaceFlowers(flowerbed, n);
+    answer = false;
+    cout << "Case " << 2 << endl
+        << "flowered:" << flowerbed << endl
+        << "n = " << n << endl
+        << "result = " << result << endl
+        << "answer = " << answer << endl
+        << endl;
+
+    return 0;
+}
