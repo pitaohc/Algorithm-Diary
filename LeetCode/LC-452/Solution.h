743c309241f01a42e5f5daadf4542b33782e0f91
pitao <pitaohc@qq.com>
2023-03-22 19:15:43 +0800

    452. 用最少数量的箭引爆气球 https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons/

---
+++ LeetCode/LC-452/Solution.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include"stdc++.h"
+
+class Solution {
+public:
+    int findMinArrowShots(std::vector<std::vector<int>>& points) {
+        using namespace std;
+        int result = 1;
+        sort(points.begin(), points.end(), [](const auto& left, const auto& right) {
+            return left[0] < right[0];
+            });
+        int right = points[0][1];
+        for (const auto& point : points)
+        {
+            if (point[0] > right)
+            {
+                result++;
+                right = point[1];
+            }
+            else
+            {
+                right = min(right, point[1]);
+            }
+        }
+        return result;
+    }
+};
---
+++ LeetCode/LC-452/main.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include"stdc++.h"
+#include"Solution.h"
+#include"util.h"
+using namespace std;
+
+
+int main()
+{
+    Solution sol;
+    vector<vector<int>> points;
+    int ans, res;
+    //case 1
+    points = { {10,16},{2,8},{1,6},{7,12} };
+    ans = 2;
+    res = sol.findMinArrowShots(points);
+    cout << "Case " << 1 << endl
+        << "Points: \n" << points << endl
+        << "Result = " << res << "\tAnswer = " << ans << endl
+        << endl;
+
+    //case 2
+    points = { {1,2},{3,4},{5,6},{7,8} };
+    ans = 4;
+    res = sol.findMinArrowShots(points);
+    cout << "Case " << 2 << endl
+        << "Points: \n" << points << endl
+        << "Result = " << res << "\tAnswer = " << ans << endl
+        << endl;
+
+    //case 3
+    points = { {1,2},{2,3},{3,4},{4,5} };
+    ans = 2;
+    res = sol.findMinArrowShots(points);
+    cout << "Case " << 3 << endl
+        << "Points: \n" << points << endl
+        << "Result = " << res << "\tAnswer = " << ans << endl
+        << endl;
+
+    return 0;
+}
