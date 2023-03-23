1b2be5437e4230a1119d3e049b617cd174e3b391
pitao <pitaohc@qq.com>
2023-03-23 09:25:04 +0800

    763. 划分字母区间 https://leetcode.cn/problems/partition-labels/

---
+++ LeetCode/LC-763/Solution.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+#include"stdc++.h"
+
+class Solution {
+public:
+    std::vector<int> partitionLabels(std::string s) {
+        using namespace std;
+        vector<vector<int>> stat(26, vector<int>(2, -1));
+        //转换成重叠区间问题
+        for (size_t i = 0; i < s.size(); i++)
+        {
+            char ch = s[i];
+            int index = ch - 'a';
+            if (stat[index][0] == -1)
+            {
+                stat[index][1] = stat[index][0] = i;
+            }
+            else
+            {
+                stat[index][1] = i;
+            }
+        }
+        //按照区间左侧排序
+        sort(stat.begin(), stat.end(), [](const auto& left, const auto& right)
+            {
+                return left[0] < right[0];
+            });
+        int left = -1, right = -1;
+        vector<int> result;
+        for (const auto& range : stat)
+        {
+            if (range[0] == -1) //不存在区间
+            {
+                continue;
+            }
+
+            if (left == -1)
+            {
+                left = range[0], right = range[1];
+            }
+            if (range[0] > right)
+            {
+                //没有交集
+                result.push_back(right - left + 1);
+                left = range[0];
+                right = range[1];
+            }
+            else
+            {
+                left = min(left, range[0]);
+                right = max(right, range[1]);
+            }
+        }
+        result.push_back(right - left + 1);
+        return result;
+    }
+};
---
+++ LeetCode/LC-763/main.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include"stdc++.h"
+#include"Solution.h"
+#include"util.h"
+using namespace std;
+
+
+int main()
+{
+    Solution sol;
+    string s;
+    vector<int> res, ans;
+
+    //case 1
+    s = "ababcbacadefegdehijhklij";
+    ans = { 9,7,8 };
+    res = sol.partitionLabels(s);
+    cout << "Case " << 1 << endl
+        << "S = " << s << endl
+        << "Answer = " << ans << endl
+        << "Result = " << res << endl
+        << endl;
+
+    //case 2
+    s = "eccbbbbdec";
+    ans = { 10 };
+    res = sol.partitionLabels(s);
+    cout << "Case " << 2 << endl
+        << "S = " << s << endl
+        << "Answer = " << ans << endl
+        << "Result = " << res << endl
+        << endl;
+
+    return 0;
+}
