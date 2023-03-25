086396a833264ed00255c7760be8eb3677579c8e
pitao <pitaohc@qq.com>
2023-03-25 16:06:30 +0800

    406. 根据身高重建队列 https://leetcode.cn/problems/queue-reconstruction-by-height/

---
+++ LeetCode/LC-406/Solution.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include"stdc++.h"
+#include"util.h"
+class Solution {
+public:
+    std::vector<std::vector<int>> reconstructQueue(std::vector<std::vector<int>>& people) {
+        using namespace std;
+        sort(people.begin(), people.end(), [](const auto& left, const auto& right)
+            {
+                if (left[0] == right[0])
+                    return left[1] < right[1];
+                else
+                    return left[0] > right[0];
+            });
+        //cout << "first sorted:" << endl;
+        //cout << people << endl;
+
+        vector<vector<int>> res;
+        for (const auto& person : people)
+        {
+            res.insert(res.begin() + person[1], person);
+        }
+        //cout << "res:\n" << res << endl;
+        return res;
+    }
+};
---
+++ LeetCode/LC-406/main.cpp
@@ -0,0 +1,67 @@
+﻿#include<iostream>
+#include"stdc++.h"
+#include"Solution.h"
+#include"util.h"
+using namespace std;
+
+
+int main()
+{
+    Solution sol;
+    vector<vector<int>> people, ans, res;
+
+    //case 1
+    people = {
+        {7,0},
+        {4,4},
+        {7,1},
+        {5,0},
+        {6,1},
+        {5,2} };
+    ans = {
+        {5,0},
+        {7,0},
+        {5,2},
+        {6,1},
+        {4,4},
+        {7,1} };
+    res = sol.reconstructQueue(people);
+    cout << "Case " << 1 << endl
+        << "People:\n"
+        << people << endl
+        << "Ans:\n"
+        << ans << endl
+        << "Res:\n"
+        << res << endl
+        << endl;
+
+    //case 2
+    people = {
+        {6,0},
+        {5,0},
+        {4,0},
+        {3,2},
+        {2,2},
+        {1,4},
+    };
+    ans = {
+        {4,0},
+        {5,0},
+        {2,2},
+        {3,2},
+        {1,4},
+        {6,0},
+    };
+    res = sol.reconstructQueue(people);
+    cout << "Case " << 2 << endl
+        << "People:\n"
+        << people << endl
+        << "Ans:\n"
+        << ans << endl
+        << "Res:\n"
+        << res << endl
+        << endl;
+
+
+    return 0;
+}
