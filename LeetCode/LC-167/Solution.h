e37b4575c8f89cc31f0e1fa94014d5d4cf823d9b
pitao <pitaohc@qq.com>
2023-03-27 14:46:07 +0800

    167. 两数之和 II - 输入有序数组 https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/

---
+++ LeetCode/LC-167/Solution.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include"stdc++.h"
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& numbers, int target) {
+        using namespace std;
+        vector<int> result(2, 0);
+        result[1] = numbers.size() - 1;
+        int& left = result[0], & right = result[1];
+        while (left < right)
+        {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target)
+            {
+                ++left;
+                ++right;
+                break;
+            }
+            else if (sum < target)
+            {
+                ++left;
+            }
+            else
+            {
+                --right;
+            }
+        }
+
+
+        return result;
+    }
+};
