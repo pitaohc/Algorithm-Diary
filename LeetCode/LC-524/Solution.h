8dbb536a4b169a39b00a575d814a760f5d38aaeb
pitaohc <pitaohc@qq.com>
2023-07-27 14:34:02 +0800

    1. LC-524 2. Create new Leetcode CMake template 3. LC-69

---
+++ LeetCode/LC-524/Solution.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include"stdc++.h"
+#include"List.h"
+
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+    string findLongestWord(string s, vector<string> &dictionary) {
+        auto sort_cmp = [](const string &s1, const string &s2) -> bool {
+            if (s1.size() == s2.size())
+                return s1 < s2;
+            else
+                return s1.size() > s2.size();
+        };
+        std::sort(dictionary.begin(), dictionary.end(), sort_cmp);
+
+        for (auto &s: dictionary) {
+            std::cout << s << std::endl;
+        }
+
+        auto find = [](const string &sub, const string &target) -> bool {
+            size_t ps = 0, pt = 0;
+            if (sub.size() > target.size()) return false;
+            while (pt < target.size()) {
+                if (sub[ps] == target[pt]) {
+                    ++ps;
+                }
+                ++pt;
+            }
+            return ps == sub.size();
+        };
+        for (auto &elem: dictionary) {
+            if (find(elem, s)) {
+                return elem;
+            }
+        }
+
+        return {};
+    }
+};
---
+++ LeetCode/LC-524/main.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include"stdc++.h"
+#include"Solution.h"
+#include"util.h"
+
+using namespace std;
+
+
+int main() {
+    Solution sol;
+    vector<string> dict;
+    string s, ans, res;
+    //case 1
+    s = "abpcplea";
+    dict = {"ale", "apple", "monkey", "plea"};
+    res = sol.findLongestWord(s, dict);
+    cout << "res: " << res << endl;
+
+    //case 2
+    s = "abpcplea";
+    dict = {"a", "b", "c"};
+    res = sol.findLongestWord(s, dict);
+    cout << "res: " << res << endl;
+
+    //case 3
+    s = "abpcplea";
+    dict = {"z", "d", "h"};
+    res = sol.findLongestWord(s, dict);
+    cout << "res: " << res << endl;
+
+    return 0;
+}
