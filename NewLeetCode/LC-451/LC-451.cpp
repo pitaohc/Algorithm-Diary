78437b93ec36f43c99e2a75d85d70dcf4134a520
pitao <pitaohc@qq.com>
2023-08-18 09:56:40 +0800

    451. 根据字符出现频率排序 https://leetcode.cn/problems/sort-characters-by-frequency/ 和347思路类似，先用hash_map统计出现频率，再使用堆按出现频率排序，最后组成字符串

---
+++ NewLeetCode/LC-451/LC-451.cpp
@@ -0,0 +1,51 @@
+﻿//
+// Created by devcpp on 23-7-25.
+//
+#include "List.h"
+#include "util.h"
+#include <iostream>
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+#include "LC-451-Solution.h"
+using namespace std;
+
+int main() {
+    Solution sol;
+    string s, res, ans;
+    int caseNum;
+
+    caseNum = 1;
+    s = "tree";
+    ans = "eert";
+    res = sol.frequencySort(s);
+    fmt::print("case {}\n"
+        "s = {}\n"
+        "ans = {}, res = {}\n\n",
+        caseNum,
+        s,
+        ans, res);
+
+    caseNum = 2;
+    s = "cccaaa";
+    ans = "cccaaa";
+    res = sol.frequencySort(s);
+    fmt::print("case {}\n"
+        "s = {}\n"
+        "ans = {}, res = {}\n\n",
+        caseNum,
+        s,
+        ans, res);
+
+    caseNum = 3;
+    s = "Aabb";
+    ans = "bbAa";
+    res = sol.frequencySort(s);
+    fmt::print("case {}\n"
+        "s = {}\n"
+        "ans = {}, res = {}\n\n",
+        caseNum,
+        s,
+        ans, res);
+
+    return 0;
+}
