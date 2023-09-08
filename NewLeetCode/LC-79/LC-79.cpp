df962cd5c72efaaafac641dc6f3a7e9f7626dbde
pitao <pitaohc@qq.com>
2023-09-08 09:01:46 +0800

    79. 单词搜索 https://leetcode.cn/problems/word-search/ 回溯，记录搜索路径

---
+++ NewLeetCode/LC-79/LC-79.cpp
@@ -0,0 +1,55 @@
+﻿#include "List.h"
+#include "util.h"
+#include <iostream>
+#include "LC-79.h"
+#include<fmt/core.h>
+#include<fmt/color.h>
+#include<fmt/ranges.h>
+using namespace std;
+
+int main() {
+    Solution sol;
+    int caseNum = 1;
+    vector<vector<char>>board;
+    string word;
+    bool ans, res;
+
+    board = { {'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'} };
+    word = "ABCCED";
+    ans = true;
+    fmt::print("Case {}\n", caseNum++);
+    fmt::print("board:\n"
+        "{}\n"
+        "word: {}\n"
+        "ans = {}, \n",
+        board, word, ans);
+    res = sol.exist(board, word);
+    fmt::print("res = {}\n", res);
+
+    board = { {'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'} };
+    word = "SEE";
+    ans = true;
+    fmt::print("Case {}\n", caseNum++);
+    fmt::print("board:\n"
+        "{}\n"
+        "word: {}\n"
+        "ans = {}, \n",
+        board, word, ans);
+    res = sol.exist(board, word);
+    fmt::print("res = {}\n", res);
+
+    board = { {'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'} };
+    word = "ABCB";
+    ans = false;
+    fmt::print("Case {}\n", caseNum++);
+    fmt::print("board:\n"
+        "{}\n"
+        "word: {}\n"
+        "ans = {}, \n",
+        board, word, ans);
+    res = sol.exist(board, word);
+    fmt::print("res = {}\n", res);
+
+
+    return 0;
+}
