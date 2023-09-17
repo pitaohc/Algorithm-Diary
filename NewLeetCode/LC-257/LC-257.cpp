0faf0d52bbd4d57fb8a5402c3973196e366ea6b4
pitao <pitaohc@qq.com>
2023-09-17 14:29:10 +0800

    257. 二叉树的所有路径 https://leetcode.cn/problems/binary-tree-paths dfs+回溯 搜索到叶子节点时，输出路径

---
+++ NewLeetCode/LC-257/LC-257.cpp
@@ -0,0 +1,33 @@
+﻿#include "List.h"
+#include "util.h"
+#include <iostream>
+#include "LC-257.h"
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <fmt/ranges.h>
+#include "Tree.h"
+#define null NULL
+using namespace std;
+
+int main() {
+    Solution sol;
+    int caseNum = 1;
+    TreeNode* root;
+    vector<string> res, ans;
+
+
+    fmt::print("Case {}\n", caseNum++);
+    root = buildTree(vector<int>{ 1, 2, 3, null, 5 });
+    res = sol.binaryTreePaths(root);
+    fmt::print("{}\n", res);
+    deleteTree(root);
+
+    fmt::print("Case {}\n", caseNum++);
+    root = buildTree(vector<int>{ 1 });
+    res = sol.binaryTreePaths(root);
+    fmt::print("{}\n", res);
+    deleteTree(root);
+
+
+    return 0;
+}
