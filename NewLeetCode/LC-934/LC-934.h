0b7e47918f95b24cc6530b4d47a78a28c77e08f6
pitao <pitaohc@qq.com>
2023-10-16 09:37:42 +0800

    934. 最短的桥 https://leetcode.cn/problems/shortest-bridge/description/

---
+++ NewLeetCode/LC-934/LC-934.h
@@ -0,0 +1,14 @@
+#pragma once
+#define DEBUG
+#include<cmath>
+#include<stdc++.h>
+#include<fmt/core.h>
+
+using namespace std;
+class Solution {
+public:
+    int shortestBridge(vector<vector<int>>& grid) {
+
+    }
+};
+
