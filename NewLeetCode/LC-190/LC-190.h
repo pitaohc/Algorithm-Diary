591da327b4be786bb8b1bd59cf7171d45c89f4d1
pitao <pitaohc@qq.com>
2023-11-14 08:41:22 +0800

    190. 颠倒二进制位 https://leetcode.cn/problems/reverse-bits 位分治 若要翻转一个二进制串，可以将其均分成左右两部分，对每部分递归执行翻转操作，然后将左半部分拼在右半部分的后面，即完成了翻转。

--- NewLeetCode/LC-190/LC-190.h
+++ NewLeetCode/LC-190/LC-190.h
@@ -6,14 +6,20 @@
 
 using namespace std;
 class Solution {
+private:
+    const uint32_t M1 = 0b01010101010101010101010101010101;
+    const uint32_t M2 = 0b00110011001100110011001100110011;
+    const uint32_t M4 = 0b00001111000011110000111100001111;
+    const uint32_t M8 = 0b00000000111111110000000011111111;
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t result = 0;
-        for (size_t i = 0; i < 32; i++)
-        {
-            result = (result << 1) | (n & 1); //result左移一位，把最低为置为n的最低位
-            n >>= 1;
-        }
-        return result;
+        n = (n >> 1 & M1) | (n & M1) << 1;
+        // abcdefgh -> 0a0c0e0g | b0d0f0h0 -> ba dc fe hg
+        n = (n >> 2 & M2) | (n & M2) << 2;
+        // badcfehg -> 00ba00fe | dc00hg00 -> dcba hgfe
+        n = (n >> 4 & M4) | (n & M4) << 4;
+        // dcbahgfe -> 0000dcba | hgfe0000 -> hgfedcba
+        n = (n >> 8 & M8) | (n & M8) << 8;
+        return n >> 16 | n << 16;
     }
 };
