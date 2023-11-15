0cedb66b695ecdbc17bff3f8ee8548a01b5e6ded
pitao <pitaohc@qq.com>
2023-11-15 09:38:04 +0800

    136. 只出现一次的数字 https://leetcode.cn/problems/single-number 使用异或进行计算

--- NewLeetCode/LC-136/LC-136.h
+++ NewLeetCode/LC-136/LC-136.h
@@ -8,17 +8,19 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int> us;
-        for (int num : nums)
+        int result = 0;
+        for (const int num : nums)
         {
-            if (us.find(num) != us.end()) { //如果已经有元素存在则删除
-                us.erase(num);
-            }
-            else //如果没有则添加
-            {
-                us.insert(num);
-            }
+            result ^= num; //按位异或
+            /*
+            思考：
+            假设只有1bit数据，我们需要找出n个1bit数据中出现一次的值
+            异或：相同的值置零，不同的值置1
+            那么 a^a = 0, a^a^0 = 0, a^a^1 = 1
+            最后的结果只跟出现一次的值有关
+            推广到32bit的数据，使用一个int存储结果，遍历数组进行异或运算，就可以得到出现一次的值
+            */
         }
-        return *us.begin(); //最后会剩下单个元素，直接返回
+        return result;
     }
 };
