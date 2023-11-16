bb7684cf0a0b2e607d74a5366f6dc06e8ab60434
pitao <pitaohc@qq.com>
2023-11-16 08:39:43 +0800

    342. 4的幂 https://leetcode.cn/problems/power-of-four 二进制表示中 1的位置

--- NewLeetCode/LC-342/LC-342.h
+++ NewLeetCode/LC-342/LC-342.h
@@ -8,13 +8,9 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n < 1) return false;
-        for (int i = 0; i < 32; i += 2) {
-            if (n == (1 << i)) {
-                return true;
-            }
-        }
-        return false;
+        return n > 0 && //n是正数
+            (n & (n - 1)) == 0 && // n是2的幂 
+            (n & 0b10101010101010101010101010101010) == 0; //n是2的偶数次幂
     }
 };
 
