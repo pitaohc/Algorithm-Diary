e5f4d6592abc817b71ba92dd63c57f92413ef868
pitao <pitaohc@qq.com>
2023-11-19 10:24:59 +0800

    448. 找到所有数组中消失的数字 https://leetcode.cn/problems/find-all-numbers-disappeared-in-an-array 使用原数组的符号位标记是否已经遍历

--- NewLeetCode/LC-448/LC-448.cpp
+++ NewLeetCode/LC-448/LC-448.cpp
@@ -10,10 +10,15 @@ using namespace std;
 int main() {
     Solution sol;
     int caseNum = 1;
-
+    vector<int> nums, res, ans;
 
     fmt::print("Case {}\n", caseNum++);
-
+    nums = { 4, 3, 2, 7, 8, 2, 3, 1 };
+    fmt::print("nums = {}\n", nums);
+    ans = { 5,6 };
+    res = sol.findDisappearedNumbers(nums);
+    fmt::print("ans = {}\n"
+        "res = {}\n", ans, res);
 
     return 0;
 }
