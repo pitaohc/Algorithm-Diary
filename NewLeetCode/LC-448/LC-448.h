0397e312359bdbf0067faa5f59fa31759652d7f9
pitao <pitaohc@qq.com>
2023-11-19 10:24:59 +0800

    448. 找到所有数组中消失的数字 https://leetcode.cn/problems/find-all-numbers-disappeared-in-an-array 使用原数组的负数标记是否已经遍历

--- NewLeetCode/LC-448/LC-448.h
+++ NewLeetCode/LC-448/LC-448.h
@@ -9,14 +9,23 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> result;
-        unordered_set<int> us;
-        for (const int num : nums) {
-            us.insert(num);
-        }
         const int n = nums.size();
-        for (int i = 1; i <= n; i++)
-        {
-            if (us.find(i) == us.end()) {
+        for (const int num : nums) {
+            int index = abs(num % n); //n存放在0上，当前遍历到的数字可能已经被置为负数
+            if (nums[index] > 0) //判断是否已经统计过
+            {
+                nums[index] = -nums[index];
+            }
+#ifdef _DEBUG
+            fmt::print("DEBUG: num = {}, nums = {}\n",num, nums);
+#endif
+
+        }
+#ifdef _DEBUG
+        fmt::print("DEBUG: {}\n", nums);
+#endif
+        for (int i = 1; i <= n; i++) {
+            if (nums[i % n] > 0) {
                 result.push_back(i);
             }
         }
