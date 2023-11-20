0fd175001248f7869d7280ace9746a09334b4ce4
pitao <pitaohc@qq.com>
2023-11-20 09:26:30 +0800

    48. 旋转图像 https://leetcode.cn/problems/rotate-image

---
+++ NewLeetCode/LC-48/LC-48.h
@@ -0,0 +1,24 @@
+#pragma once
+#define DEBUG
+#include<cmath>
+#include<stdc++.h>
+#include<fmt/core.h>
+
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = i; j < n - i - 1; j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+};
