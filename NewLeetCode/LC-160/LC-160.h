fa702af2480639bb6c191b4cda2beaf8ac23e660
pitao <pitaohc@qq.com>
2023-12-16 17:18:21 +0800

    160. 相交链表 https://leetcode.cn/problems/intersection-of-two-linked-lists/description/ 交换遍历 a+c+b == b+c+a

---
+++ NewLeetCode/LC-160/LC-160.h
@@ -0,0 +1,27 @@
+#pragma once
+#define DEBUG
+#include<cmath>
+#include<stdc++.h>
+#include<fmt/core.h>
+
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        using Node = ListNode*;
+        Node curA = headA, curB = headB;
+        while (curA !=curB) {
+            curA = curA ? curA->next : headB;
+            curB = curB ? curB->next : headA;
+        }
+        return curA;
+    }
+};
