54bc6f57725f7125665e2b433c493fa64bfa167a
pitao <pitaohc@qq.com>
2023-12-25 00:49:27 +0800

    637. 二叉树的层平均值 https://leetcode.cn/problems/average-of-levels-in-binary-tree/description/ 层次遍历，记住每层的节点个数并累加sum

---
+++ NewLeetCode/LC-637/LC-637.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <fmt/ranges.h>
+#include "LC-637.h"
+#include "List.h"
+#include "util.h"
+#include "Tree.h"
+using namespace std;
+
+int main() {
+    Solution sol;
+    int caseNum = 1;
+
+
+    fmt::print("Case {}\n", caseNum++);
+
+
+    return 0;
+}
