f748cd8c5e6b2db5986caebc1856d22c3bbb7177
pitao <pitaohc@qq.com>
2024-01-11 10:17:29 +0800

    787. K 站中转内最便宜的航班 https://leetcode.cn/problems/cheapest-flights-within-k-stops/description/ 动态规划,一维滚动数组状态转移

--- NewLeetCode/LC-787/LC-787.h
+++ NewLeetCode/LC-787/LC-787.h
@@ -12,15 +12,18 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         constexpr int INF = INT_MAX / 2;
-        vector<vector<int>> dist(n, vector<int>(k + 2, INF));
-        dist[src][0] = 0;
+        vector<int> dist(n, INF);
+        dist[src] = 0;
+        int minCost = INF;
         for (int turn = 1; turn <= k + 1; ++turn) {
+            vector<int> newDist(n, INF);
             for (const auto& flight : flights) {
                 int from = flight[0], to = flight[1], cost = flight[2];
-                dist[to][turn] = min(dist[to][turn], dist[from][turn - 1] + cost);
+                newDist[to] = min(newDist[to], dist[from] + cost);
             }
+            dist = move(newDist);
+            minCost = min(minCost, dist[dst]);
         }
-        int minCost = *min_element(dist[dst].begin(), dist[dst].end());
         return minCost != INF ? minCost : -1;
     }
 };
